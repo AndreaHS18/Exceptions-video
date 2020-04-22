1db2b064dd96af860b57b54f19f80845a2ca4694
AndreaHS18 <60455549+AndreaHS18@users.noreply.github.com>
2020-04-22 17:48:44 -0500

    Add files via upload

---
+++ Exceptions-video/src/exceptions/examples/SaveProgressException.java
@@ -0,0 +1,8 @@
+package exceptions.examples;
+
+public class SaveProgressException extends Exception{
+
+    public SaveProgressException(Throwable cause){
+        super(cause);
+    }
+}
---
+++ Exceptions-video/src/exceptions/examples/SaveStatisticsException.java
@@ -0,0 +1,8 @@
+package exceptions.examples;
+
+public class SaveStatisticsException extends Exception {
+
+    public SaveStatisticsException (Throwable cause){
+        super(cause);
+    }
+}
